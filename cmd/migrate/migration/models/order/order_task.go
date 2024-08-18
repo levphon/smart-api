59f3799fe4e0d463f6513e78cc408b2b2365a5c0
sunwenbo <wenbo.sun@zhipuai.cn>
2024-08-19 00:14:01 +0800

    开发任务中心 增删改查接口

--- cmd/migrate/migration/models/order/order_task.go
+++ cmd/migrate/migration/models/order/order_task.go
@@ -7,7 +7,7 @@ import "go-admin/cmd/migrate/migration/models"
 type OrderTask struct {
 	ID          int    `gorm:"primaryKey;autoIncrement" json:"id"`
 	Name        string `gorm:"column:name; type: varchar(256)" json:"name" form:"name"`                      // 任务名称
-	TaskType    string `gorm:"column:taskType; type: varchar(45)" json:"task_type" form:"task_type"`         // 任务类型
+	TaskType    string `gorm:"column:taskType; type: varchar(45)" json:"taskType" form:"task_type"`          // 任务类型
 	Interpreter string `gorm:"column:interpreter; type: varchar(100)" json:"interpreter" form:"interpreter"` // 解释器
 	Content     string `gorm:"column:content; type: longtext" json:"content" form:"content"`                 // 任务内容
 	Creator     string `gorm:"column:creator; type: varchar(45)" json:"creator" form:"creator"`              // 创建者
